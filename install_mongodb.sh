3c883aae2c73c8e20dbf43c9c0c6ffa9d9659a23
Vitaliy Gutorov <gutorov.v@gmail.com>
2020-12-18 00:28:59 +0300

    #HW4 fix uptadte

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt-get -y update
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get -y update
