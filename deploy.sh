23e7231bfacd3484121552e1e0f8518f73cfd50e
Vitaliy Gutorov <gutorov.v@gmail.com>
2020-12-17 23:11:09 +0300

    #HW4 cloudtestapp script

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt -y  install git
+git clone -b monolith https://github.com/express42/reddit.git
+cd reddit && bundle install
+puma -d
+ps aux | grep puma
---
+++ install_ruby.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt -y update
+sudo apt -y install ruby-full ruby-bundler build-essential
